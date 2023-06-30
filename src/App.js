dd2202c505428042f55984ace52a310baa1701a0
Ajinkya-Mane <ajinkyamane.react@gmail.com>
2023-06-30 14:19:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addUser, deleteUser, updateUsername } from "./Features/Users";
+
+
 
 function App() {
+
+  const dispatch = useDispatch();
+  const userList = useSelector((state) => state.users.value);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [newName, setNewName] = useState("");
+  const [newEmail, setNewEmail] = useState("");
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container mt-4">
+      
+      <div className="container mt-3 mb-3">
+        <input
+          type="text"
+          placeholder="Enter Name..."
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <input
+          type="text" className="ms-2"
+          placeholder="Enter Email..."
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+        />
+        <button className="btn btn-success ms-2"
+          onClick={() => {
+            
+            dispatch(
+              addUser({
+                id: userList[userList.length - 1].id + 1,
+                name,
+                email,
+              })
+            );
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add User
+        </button>
+      </div>
+      <div className="container mt-3 mb-3">
+      
+      <table className="table table-bordered table-hover">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>User Name</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+        {userList.map((user, i) => {
+            return (
+              <tr key={i}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                <input
+                type="text"
+                placeholder="Enter New Name..."
+                onChange={(event) => {
+                  setNewName(event.target.value);
+                }}
+              />
+               <input
+                type="text"
+                placeholder="Enter New Email..."
+                onChange={(event) => {
+                  setNewEmail(event.target.value);
+                }}
+              /><button className="btn btn-primary btn-sm me-4 ms-3" onClick={() => {
+                  dispatch(
+                    updateUsername({ id: user.id, name: newName, email : newEmail })
+                  );
+                }}>
+                    Update
+                  </button>
+                  <button className="btn btn-danger btn-sm me-3" onClick={() => {
+                  dispatch(deleteUser({ id: user.id }));
+                }}>Delete</button>
+                </td>
+              </tr>
+            );
+          })}
+
+
+
+        </tbody>
+        </table>
+       
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import { configureStore } from '@reduxjs/toolkit';
+import usersReducer from "./Features/Users";
+
+const store = configureStore({
+  reducer: {
+    users: usersReducer,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+   <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
